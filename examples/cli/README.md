93bc0903fdfe103bd49a88eed452ca532edcf39c
Arne Bahlo <arne@axiom.co>
2022-08-05 14:15:54 +0200

    Add CLI example
    This example showcases different ways to use the individual functions of
    this library.

---
+++ examples/cli/README.md
@@ -0,0 +1,19 @@
+# cli
+
+This example implements a very basic CLI for [Axiom](https://axiom.co).
+
+> **Warning**: This is meant to show some examples on how to call the various methods.
+> Please don't actually use this, we have an 
+> [official CLI](https://github.com/axiomhq/cli).
+
+## Prerequisites
+
+You'll need an account at [Axiom](https://cloud.axiom.co) and a personal token.
+
+## Start the example
+
+```sh
+export AXIOM_TOKEN=<your personal token>
+export AXIOM_ORG_ID=<your axiom org id>
+cargo run
+```
---
+++ examples/cli/src/main.rs
@@ -0,0 +1,122 @@
+use axiom_rs::{
+    datasets::{ContentEncoding, ContentType},
+    Client,
+};
+use std::time::Duration;
+use structopt::StructOpt;
+use tokio::io::{stdin, AsyncReadExt};
+
+#[derive(Debug, StructOpt)]
+enum Opt {
+    /// Work with users.
+    Users(Users),
+    /// Manipulate datasets.
+    Datasets(Datasets),
+}
+
+#[derive(Debug, StructOpt)]
+enum Users {
+    /// Get the current user
+    Current,
+}
+
+#[derive(Debug, StructOpt)]
+enum Datasets {
+    /// List datasets
+    List,
+    /// Get a dataset
+    Get { name: String },
+    /// Get information for a dataset
+    Info { name: String },
+    /// Update the description of a dataset
+    Update {
+        name: String,
+
+        #[structopt(long, short)]
+        description: String,
+    },
+    /// Delete a dataset
+    Delete { name: String },
+    /// Trim a dataset
+    Trim {
+        name: String,
+
+        #[structopt(long)]
+        seconds: u64,
+    },
+    /// Ingest into a dataset from stdin.
+    Ingest {
+        name: String,
+
+        #[structopt(long, default_value = "application/json")]
+        content_type: ContentType,
+        #[structopt(long, default_value = "")]
+        content_encoding: ContentEncoding,
+    },
+    /// Query a dataset using APL.
+    Query { apl: String },
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let opt = Opt::from_args();
+    let client = Client::new()?;
+
+    match opt {
+        Opt::Datasets(datasets) => match datasets {
+            Datasets::List => client
+                .datasets
+                .list()
+                .await?
+                .into_iter()
+                .for_each(|dataset| {
+                    println!("{:?}", dataset);
+                }),
+            Datasets::Get { name } => println!("{:?}", client.datasets.get(&name).await?),
+            Datasets::Info { name } => println!("{:?}", client.datasets.info(&name).await?),
+            Datasets::Update { name, description } => {
+                let dataset = client
+                    .datasets
+                    .update(
+                        &name,
+                        axiom_rs::datasets::DatasetUpdateRequest { description },
+                    )
+                    .await?;
+                println!("{:?}", dataset);
+            }
+            Datasets::Delete { name } => client.datasets.delete(&name).await?,
+            Datasets::Trim { name, seconds } => println!(
+                "{:?}",
+                client
+                    .datasets
+                    .trim(&name, Duration::from_secs(seconds))
+                    .await?
+            ),
+            Datasets::Ingest {
+                name,
+                content_type,
+                content_encoding,
+            } => {
+                let mut buf = Vec::new();
+                stdin().read_to_end(&mut buf).await?;
+                let ingest_status = client
+                    .datasets
+                    .ingest_raw(&name, buf, content_type, content_encoding)
+                    .await?;
+                println!("{:?}", ingest_status);
+            }
+            Datasets::Query { apl } => {
+                let result = client.datasets.apl_query(apl, None).await?;
+                println!("{:?}", result);
+            }
+        },
+        Opt::Users(users) => match users {
+            Users::Current => {
+                let user = client.users.current().await?;
+                println!("{:?}", user);
+            }
+        },
+    };
+
+    Ok(())
+}
