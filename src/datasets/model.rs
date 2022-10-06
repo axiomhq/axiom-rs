672122bb23e756d4bbf1caabbc1f98468dd14e24
Arne Bahlo <arne@axiom.co>
2022-10-06 11:15:57 +0200

    Add pearson, rate aggregations

--- src/datasets/model.rs
+++ src/datasets/model.rs
@@ -487,7 +487,6 @@ pub enum AggregationOp {
     Count,
     CountDistinct,
     MakeSet,
-    MakeSetIf,
 
     // Only works for numbers.
     Sum,
@@ -506,6 +505,9 @@ pub enum AggregationOp {
     // the APL query result.
     CountIf,
     DistinctIf,
+    MakeSetIf,
+    Rate,
+    Pearson,
 
     Unknown(String),
 }
@@ -519,7 +521,6 @@ impl Serialize for AggregationOp {
             Self::Count => "count",
             Self::CountDistinct => "distinct",
             Self::MakeSet => "makeset",
-            Self::MakeSetIf => "makesetif",
             Self::Sum => "sum",
             Self::Avg => "avg",
             Self::Min => "min",
@@ -533,6 +534,9 @@ impl Serialize for AggregationOp {
             Self::ArgMax => "argmax",
             Self::CountIf => "countif",
             Self::DistinctIf => "distinctif",
+            Self::MakeSetIf => "makesetif",
+            Self::Rate => "rate",
+            Self::Pearson => "pearson_correlation",
             Self::Unknown(ref s) => s,
         })
     }
@@ -555,7 +559,6 @@ impl<'de> Visitor<'de> for AggregationOpVisitor {
             "count" => Ok(Self::Value::Count),
             "distinct" => Ok(Self::Value::CountDistinct),
             "makeset" => Ok(Self::Value::MakeSet),
-            "makesetif" => Ok(Self::Value::MakeSetIf),
             "sum" => Ok(Self::Value::Sum),
             "avg" => Ok(Self::Value::Avg),
             "min" => Ok(Self::Value::Min),
@@ -569,6 +572,9 @@ impl<'de> Visitor<'de> for AggregationOpVisitor {
             "argmax" => Ok(Self::Value::ArgMax),
             "countif" => Ok(Self::Value::CountIf),
             "distinctif" => Ok(Self::Value::DistinctIf),
+            "makesetif" => Ok(Self::Value::MakeSetIf),
+            "rate" => Ok(Self::Value::Rate),
+            "pearson_correlation" => Ok(Self::Value::Pearson),
             aggregation => Ok(Self::Value::Unknown(aggregation.to_string())),
         }
     }
